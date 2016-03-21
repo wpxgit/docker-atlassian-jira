76790adce4d047c86323e1694c9386e692b4f2c9
wpxgit <michael@seoviu.com>
2016-03-21 15:56:34 +0000

    JIRA

--- scripts/ensure-gcloud-installed.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-pip install pyopenssl
-if [ ! -d ~/google-cloud-sdk ]; then
-    curl https://sdk.cloud.google.com | bash;
-    ~/google-cloud-sdk/bin/gcloud components update preview
-fi
-
